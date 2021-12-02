951a546f7fef2bab37d0dee0da2f9558d1ac2f74
Rafiur Rahman <rrfoundary@gmail.com>
2021-12-02 22:08:50 +0600

    ads - structuring the header section

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Search from "./common/search";
+import "../styles/header.scss";
+import "../styles/dropdown-cart.scss";
 function Header() {
   return (
     <header class="header-part">
@@ -19,37 +22,7 @@ function Header() {
               <i class="fas fa-search"></i>
             </button>
           </div>
-          <form class="header-form">
-            <div class="header-search">
-              <button type="submit" title="Search Submit ">
-                <i class="fas fa-search"></i>
-              </button>
-              <input type="text" placeholder="Search, Whatever you needs..." />
-              <button type="button" title="Search Option" class="option-btn">
-                <i class="fas fa-sliders-h"></i>
-              </button>
-            </div>
-            <div class="header-option">
-              <div class="option-grid">
-                <div class="option-group">
-                  <input type="text" placeholder="City" />
-                </div>
-                <div class="option-group">
-                  <input type="text" placeholder="State" />
-                </div>
-                <div class="option-group">
-                  <input type="text" placeholder="Min Price" />
-                </div>
-                <div class="option-group">
-                  <input type="text" placeholder="Max Price" />
-                </div>
-                <button type="submit">
-                  <i class="fas fa-search"></i>
-                  <span>Search</span>
-                </button>
-              </div>
-            </div>
-          </form>
+          <Search />
           <div class="header-right">
             <ul class="header-list">
               <li class="header-item">
