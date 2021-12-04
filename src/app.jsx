f131421f7bd09d9a541d8a4763de796d20e3e3f7
Rafiur Rahman <rrfoundary@gmail.com>
2021-12-04 18:57:36 +0600

    ads - added footer component in all pages

--- src/app.jsx
+++ src/app.jsx
@@ -14,6 +14,7 @@ import CityHome from "./components/cityHome";
 import IncrementCounter from "./components/common/incrementCounter";
 import PostAds from "./components/postAds";
 import PostHome from "./components/postHome";
+import Footer from "./components/footer";
 
 function App() {
   const [isOpenAside, setIsOpenAside] = useToggle(false);
@@ -45,6 +46,7 @@ function App() {
       <CityHome />
       <PostAds />
       <PostHome />
+      <Footer />
     </div>
   );
 }
