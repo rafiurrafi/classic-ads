5e8b429483addef130961c034c181af045aba6ee
Rafiur Rahman <rrfoundary@gmail.com>
2021-12-04 20:29:43 +0600

    ads - added category service for category

--- src/components/postHome.jsx
+++ src/components/postHome.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import $ from "jquery";
+// import $ from "jquery";
 const PostHome = () => {
   // React.useEffect(() => {
   //   $(".blog-slider").slick({
---
+++ src/services/categoryService.js
@@ -0,0 +1,99 @@
+import * as Ai from "react-scripts/ai";
+import * as Bi from "react-scripts/bi";
+import * as Ri from "react-scripts/ri";
+import * as Gi from "react-scripts/gi";
+import * as Fa from "react-scripts/fa";
+import * as Bs from "react-icons/bs";
+const categories = [
+  {
+    _id: 1,
+    name: "Mobile",
+    icon: <Bi.BiMobileAlt />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 2,
+    name: "Electronics",
+    icon: <Ri.RiComputerLine />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 3,
+    name: "Home &amp; Livings",
+    icon: <Ai.AiOutlineHome />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 4,
+    name: "Vehicles",
+    icon: <Ai.AiFillCar />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 5,
+    name: "Property",
+    icon: <Ai.AiOutlinePropertySafety />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 6,
+    name: "Pet & Animale",
+    icon: <Fa.FaCat />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 7,
+    name: "Fashion & Beauty",
+    icon: <Bs.BsSmartwatch />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 14,
+    name: "Hobbies, Sports & Kids",
+    icon: <Bi.BiFootball />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 8,
+    name: "Industry & Business",
+    icon: <Fa.FaIndustry />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 15,
+    name: "Education",
+    icon: <Bs.BsBookHalf />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 9,
+    name: "Essentils",
+    icon: <Gi.GiMaterialsScience />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 10,
+    name: "Jobs",
+    icon: <Bs.BsFillHandbagFill />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 11,
+    name: "Service",
+    icon: <Fa.FaTools />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 12,
+    name: "Agriculture",
+    icon: <Gi.GiFoodChain />,
+    numberOfAds: 21211,
+  },
+  {
+    _id: 13,
+    name: "Overseas jobs",
+    icon: <Gi.GiCommercialAirplane />,
+    numberOfAds: 21211,
+  },
+];
+export default categories;
