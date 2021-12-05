791a677fd114d4a92227876bc63d49fe62e3ab76
Rafiur Rahman <rrfoundary@gmail.com>
2021-12-05 14:02:34 +0600

    ads - added react animated slider as experiment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import ReactDOM from "react-dom";
-import App from "./app";
-ReactDOM.render(<App />, document.querySelector("#root"));
+import Slider from "react-slick";
+// import App from "./app";
+// ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<Slider />, document.querySelector("#root"));
