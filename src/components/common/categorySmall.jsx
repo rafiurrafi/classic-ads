22b144f31aba76423b55aea8245e3c95cfc1a66b
Rafiur Rahman <rrfoundary@gmail.com>
2021-12-06 13:21:55 +0600

    ads - added subcategory in category service

--- src/components/common/categorySmall.jsx
+++ src/components/common/categorySmall.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import categories from "../../services/categoryService";
+import useToggle from "../hooks/ustToggle";
 import SubcategorySmall from "./subcategorySmall";
 const CategorySmall = (props) => {
+  const [isOpenSub, setIsOpenSub] = useToggle(false);
   return (
     <section class="section suggest-part suggest-part-small">
       <div class="container">
         <div class="suggest-slider slider-arrow row">
           {categories.map((category) => (
-            <div className="col-3 ">
-              <a href="ad-list-column3.html" class="suggest-card">
+            <div className="col-3" onClick={setIsOpenSub}>
+              <a class="suggest-card">
                 <img src={category.icon} alt="car" />
                 <h6>{category.name}</h6>
               </a>
+              {isOpenSub && (
+                <SubcategorySmall
+                  subcategories={category.subcategory}
+                  onIsOpenSub={setIsOpenSub}
+                />
+              )}
             </div>
           ))}
         </div>
         {/* end of suggest slider  */}
-        <SubcategorySmall />
       </div>
     </section>
   );
--- src/components/common/subcategorySmall.jsx
+++ src/components/common/subcategorySmall.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { AiOutlineLeft } from "react-icons/ai";
-const SubcategorySmall = () => {
+const SubcategorySmall = ({ subcategories, onIsOpenSub }) => {
   return (
     <div className="subcategory">
       <div className="subcategory__header">
         <div className="subcategory__input-container">
-          <button className="subcategory__btn">
+          <button className="subcategory__btn" onClick={onIsOpenSub}>
             <AiOutlineLeft />
           </button>
           <input
@@ -15,7 +15,21 @@ const SubcategorySmall = () => {
           />
         </div>
       </div>
-      <div className="subcategory__content">Content</div>
+      <div className="subcategory__content">
+        {subcategories.map((subcategory) => (
+          <div key={subcategory._id} className="subcategory__content-wrapper">
+            <img
+              src={subcategory.icon}
+              alt="subcategory"
+              className="subcategory__content-img"
+            />
+            <div className="subcategory__content-text">
+              <h4>{subcategory.title}</h4>
+              <p>{subcategory.numberOfAds} ads</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
