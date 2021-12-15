ef305ff9ecc31ffd7439632040c102fbc2c38b81
Rafiur Rahman <rrfoundary@gmail.com>
2021-12-15 13:01:39 +0600

    ads - added sidebar tab functionality

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -2,7 +2,15 @@ import React from "react";
 const Banner = (props) => {
   return (
     <section className="banner-part">
-    
+      <div className="container">
+        <div className="banner-content">
+          <h1>You can #Buy, #Rent, #Booking anything from here.</h1>
+          <p>
+            Buy and sell everything from used cars to mobile phones and
+            computers, or search for property, jobs and more in the world.
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/sidebarProfile.jsx
+++ src/components/sidebarProfile.jsx
@@ -1,7 +1,203 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Fa from "react-icons/fa";
 
+const MainMenuPanel = () => {
+  return (
+    <div className="tab-pane active" id="main-menu">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <a className="navbar-link" href="index.html">
+            Home
+          </a>
+        </li>
+        <li className="navbar-item navbar-dropdown">
+          <a className="navbar-link" href="#!">
+            <span>Categories</span>
+            <Fa.FaPlus />
+          </a>
+          <ul className="dropdown-list">
+            <li>
+              <a className="dropdown-link" href="category-list.html">
+                category list
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-link" href="category-details.html">
+                category details
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className="navbar-item navbar-dropdown">
+          <a className="navbar-link" href="#!">
+            <span>Advertise List</span>
+            <Fa.FaPlus />
+          </a>
+          <ul className="dropdown-list">
+            <li>
+              <a className="dropdown-link" href="ad-list-column3.html">
+                ad list column 3
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-link" href="ad-list-column2.html">
+                ad list column 2
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-link" href="ad-list-column1.html">
+                ad list column 1
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className="navbar-item navbar-dropdown">
+          <a className="navbar-link" href="#!">
+            <span>Advertise details</span>
+            <Fa.FaPlus />
+          </a>
+          <ul className="dropdown-list">
+            <li>
+              <a className="dropdown-link" href="ad-details-grid.html">
+                ad details grid
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-link" href="ad-details-left.html">
+                ad details left
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-link" href="ad-details-right.html">
+                ad details right
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className="navbar-item navbar-dropdown">
+          <a className="navbar-link" href="#!">
+            <span>Pages</span>
+            <Fa.FaPlus />
+          </a>
+          <ul className="dropdown-list">
+            <li>
+              <a className="dropdown-link" href="about.html">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-link" href="compare.html">
+                Ad Compare
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-link" href="cities.html">
+                Ad by Cities
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-link" href="price.html">
+                Pricing Plan
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-link" href="user-form.html">
+                User Form
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-link" href="404.html">
+                404
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className="navbar-item navbar-dropdown">
+          <a className="navbar-link" href="#!">
+            <span>blogs</span>
+            <i className="fas fa-plus"></i>
+          </a>
+          <ul className="dropdown-list">
+            <li>
+              <a className="dropdown-link" href="blog-list.html">
+                Blog list
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-link" href="blog-details.html">
+                blog details
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className="navbar-item">
+          <a className="navbar-link" href="contact.html">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+const AuthorMenu = () => {
+  return (
+    <div className="tab-pane active" id="author-menu">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <a className="navbar-link" href="dashboard.html">
+            Dashboard
+          </a>
+        </li>
+        <li className="navbar-item">
+          <a className="navbar-link" href="profile.html">
+            Profile
+          </a>
+        </li>
+        <li className="navbar-item">
+          <a className="navbar-link" href="ad-post.html">
+            Ad Post
+          </a>
+        </li>
+        <li className="navbar-item">
+          <a className="navbar-link" href="my-ads.html">
+            My Ads
+          </a>
+        </li>
+        <li className="navbar-item">
+          <a className="navbar-link" href="setting.html">
+            Settings
+          </a>
+        </li>
+        <li className="navbar-item navbar-dropdown">
+          <a className="navbar-link" href="bookmark.html">
+            <span>bookmark</span>
+            <span>0</span>
+          </a>
+        </li>
+        <li className="navbar-item navbar-dropdown">
+          <a className="navbar-link" href="message.html">
+            <span>Message</span>
+            <span>0</span>
+          </a>
+        </li>
+        <li className="navbar-item navbar-dropdown">
+          <a className="navbar-link" href="notification.html">
+            <span>Notification</span>
+            <span>0</span>
+          </a>
+        </li>
+        <li className="navbar-item">
+          <a className="navbar-link" href="user-form.html">
+            Logout
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 const SidebarProfile = ({ isOpenAside, onOpenAside }) => {
+  const [showMenus, setShowMenus] = useState("main");
+
   return (
     <aside className={`sidebar-part ${isOpenAside && "active"}`}>
       <div className="sidebar-body">
@@ -31,209 +227,26 @@ const SidebarProfile = ({ isOpenAside, onOpenAside }) => {
           <div className="sidebar-menu">
             <ul className="nav nav-tabs">
               <li>
-                <a
-                  href="#main-menu"
-                  className="nav-link active"
-                  data-toggle="tab"
+                <button
+                  className={`nav-link ${showMenus === "main" ? "active" : ""}`}
+                  onClick={() => setShowMenus("main")}
                 >
                   Main Menu
-                </a>
+                </button>
               </li>
               <li>
-                <a href="#author-menu" className="nav-link" data-toggle="tab">
+                <button
+                  className={`nav-link ${
+                    showMenus === "author" ? "active" : ""
+                  }`}
+                  onClick={() => setShowMenus("author")}
+                >
                   Author Menu
-                </a>
+                </button>
               </li>
             </ul>
-
-            <div className="tab-pane active" id="main-menu">
-              <ul className="navbar-list">
-                <li className="navbar-item">
-                  <a className="navbar-link" href="index.html">
-                    Home
-                  </a>
-                </li>
-                <li className="navbar-item navbar-dropdown">
-                  <a className="navbar-link" href="#!">
-                    <span>Categories</span>
-                    <i className="fas fa-plus"></i>
-                    <Fa.FaPlus />
-                  </a>
-                  <ul className="dropdown-list">
-                    <li>
-                      <a className="dropdown-link" href="category-list.html">
-                        category list
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-link" href="category-details.html">
-                        category details
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="navbar-item navbar-dropdown">
-                  <a className="navbar-link" href="#!">
-                    <span>Advertise List</span>
-                    <Fa.FaPlus />
-                  </a>
-                  <ul className="dropdown-list">
-                    <li>
-                      <a className="dropdown-link" href="ad-list-column3.html">
-                        ad list column 3
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-link" href="ad-list-column2.html">
-                        ad list column 2
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-link" href="ad-list-column1.html">
-                        ad list column 1
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="navbar-item navbar-dropdown">
-                  <a className="navbar-link" href="#!">
-                    <span>Advertise details</span>
-                    <Fa.FaPlus />
-                  </a>
-                  <ul className="dropdown-list">
-                    <li>
-                      <a className="dropdown-link" href="ad-details-grid.html">
-                        ad details grid
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-link" href="ad-details-left.html">
-                        ad details left
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-link" href="ad-details-right.html">
-                        ad details right
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="navbar-item navbar-dropdown">
-                  <a className="navbar-link" href="#!">
-                    <span>Pages</span>
-                    <Fa.FaPlus />
-                  </a>
-                  <ul className="dropdown-list">
-                    <li>
-                      <a className="dropdown-link" href="about.html">
-                        About Us
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-link" href="compare.html">
-                        Ad Compare
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-link" href="cities.html">
-                        Ad by Cities
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-link" href="price.html">
-                        Pricing Plan
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-link" href="user-form.html">
-                        User Form
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-link" href="404.html">
-                        404
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="navbar-item navbar-dropdown">
-                  <a className="navbar-link" href="#!">
-                    <span>blogs</span>
-                    <i className="fas fa-plus"></i>
-                  </a>
-                  <ul className="dropdown-list">
-                    <li>
-                      <a className="dropdown-link" href="blog-list.html">
-                        Blog list
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-link" href="blog-details.html">
-                        blog details
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="navbar-item">
-                  <a className="navbar-link" href="contact.html">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="tab-pane" id="author-menu">
-              <ul className="navbar-list">
-                <li className="navbar-item">
-                  <a className="navbar-link" href="dashboard.html">
-                    Dashboard
-                  </a>
-                </li>
-                <li className="navbar-item">
-                  <a className="navbar-link" href="profile.html">
-                    Profile
-                  </a>
-                </li>
-                <li className="navbar-item">
-                  <a className="navbar-link" href="ad-post.html">
-                    Ad Post
-                  </a>
-                </li>
-                <li className="navbar-item">
-                  <a className="navbar-link" href="my-ads.html">
-                    My Ads
-                  </a>
-                </li>
-                <li className="navbar-item">
-                  <a className="navbar-link" href="setting.html">
-                    Settings
-                  </a>
-                </li>
-                <li className="navbar-item navbar-dropdown">
-                  <a className="navbar-link" href="bookmark.html">
-                    <span>bookmark</span>
-                    <span>0</span>
-                  </a>
-                </li>
-                <li className="navbar-item navbar-dropdown">
-                  <a className="navbar-link" href="message.html">
-                    <span>Message</span>
-                    <span>0</span>
-                  </a>
-                </li>
-                <li className="navbar-item navbar-dropdown">
-                  <a className="navbar-link" href="notification.html">
-                    <span>Notification</span>
-                    <span>0</span>
-                  </a>
-                </li>
-                <li className="navbar-item">
-                  <a className="navbar-link" href="user-form.html">
-                    Logout
-                  </a>
-                </li>
-              </ul>
-            </div>
+            {showMenus === "main" && <MainMenuPanel />}
+            {showMenus === "author" && <AuthorMenu />}
           </div>
 
           <div className="sidebar-footer">
