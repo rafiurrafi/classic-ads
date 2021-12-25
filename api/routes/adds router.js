778a81f75c94c965ad3d013705ce5f8e2267b7fe
Md Iftakher Hossain <67021268+iftakher-t@users.noreply.github.com>
2021-12-25 21:57:58 +0600

    Update adds router.js

--- api/routes/adds router.js
+++ api/routes/adds router.js
@@ -26,6 +26,33 @@ router.post("/", async (req, res) => {
   }
 
 });
+//update a post
+
+router.put("/:id", async (req, res) => {
+
+  try {
+
+    const ads = await Ads.findById(req.params.id);
+
+    if (post.userId === req.body.userId) {
+
+      await ads.updateOne({ $set: req.body });
+
+      res.status(200).json("the ads has been updated");
+
+    } else {
+
+      res.status(403).json("you can update only your post");
+
+    }
+
+  } catch (err) {
+
+    res.status(500).json(err);
+
+  }
+
+});
 // getbyId
 // Search adds
 
