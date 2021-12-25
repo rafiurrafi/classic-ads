cf42365c9ebca6812eb0f17b7f53f735cd8f44d5
Md Iftakher Hossain <67021268+iftakher-t@users.noreply.github.com>
2021-12-25 21:42:52 +0600

    Update adds router.js

--- api/routes/adds router.js
+++ api/routes/adds router.js
@@ -1,4 +1,33 @@
 // create
 // getbyId
 // Search adds
+const express = require('express');
+
+const router = express.Router();
+const Ads = require('../models/Ads');
+const Sequelize = require ('sequelize')
+
+const Op = Sequelize.Op
+
+router.get('/search',(req,res) =>{
+
+let {term }=req.query
+
+term = term.toLowerCase();
+
+Ads.findAll({where : {
+
+city: {[Op.likes]: '%'+ term +'%' }} 
+
+})
+
+.then( ads => res.render ('ads',{ads})
+
+.catch (err => console.log(err))
+
+});
+
+
+
+
 //get all adds
