6b5e7d38a956aa57c535f518368095b1ee33902d
Md Iftakher Hossain <67021268+iftakher-t@users.noreply.github.com>
2022-01-10 15:15:04 +0600

    Update Category.js

--- api/models/Category.js
+++ api/models/Category.js
@@ -3,21 +3,18 @@ const {Schema, model}= require('mongoose')
 const categorySchema = new Schema({  // this is for product
 
     categoryName:String,
-
+    freeAdsLimit:{
+type: Number,
+default:5
+}
     
 
     product:[{ 
-
         type:Schema.Types.ObjectId,
-
         ref: 'Product',
-
     }],
-
   },{
-
     timestamps:true
-
 })
 
 module.exports = model('Category', categorySchema)
